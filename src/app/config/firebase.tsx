997e699e1bb4530506ef46049797ec89ac43cdbd
MiguelAssis0 <miganjos88@gmail.com>
2025-02-03 15:42:49 -0300

    .

--- src/app/config/firebase.tsx
+++ src/app/config/firebase.tsx
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-  appId: process.env.NEXT_PUBLIC_APPID
+  apiKey: "AIzaSyDncfByTuk4TvukEq4pAEISwQ9ctqC4iSk",
+  authDomain: "ecoblog-9f040.firebaseapp.com",
+  projectId: "ecoblog-9f040",
+  storageBucket: "ecoblog-9f040.firebasestorage.app",
+  messagingSenderId: "1076088328205",
+  appId: "1:1076088328205:web:4e2ee19a1721f8d9d1c544"
 };
 
 // Initialize Firebase
